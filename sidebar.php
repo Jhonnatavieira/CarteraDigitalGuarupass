00eeb439e9e9b664e4e9645c30aba4f12c0e1e5d
Jhonn-Vieira <jhonnatavieiradesouza124@gmail.com>
2025-08-02 11:19:26 -0300

    feat(dashboard): cria o arquivo de dashboard e cria o corpo inicial
    altera o conteudo da sidebar, retirando o conteudo de suporte a navbar deixando somente o necessario no arquivo da sidebar.
    Além disso, o conteudo de suporte da sidebar é colocado dentro do arquivo da dashboard.

--- sidebar.php
+++ sidebar.php
@@ -1,36 +1,27 @@
-<!doctype html>
-<html lang="pt-br">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>sidebar</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
-  </head>
-  <body>
-    <div class="d-flex">
-        <!-- sidebar -->
-         <aside id="sidebar">
-            <div class="sidebar-logo">
-                <a href="#">Guarupass</a>
-            </div>
+<!-- sidebar -->
+         
+<div class="sidebar-logo">
+    <a href="#">Guarupass</a>
+</div>
 
-            <!-- navegação da sidebar -->
-             <ul class="sidebar-nav p-0">
-                <!-- topo da navegação -->
-                <li class="sidebar-header">
-                    Paginas
-                </li>
-                <!-- itens da navegação -->
-                 <li class="sidebar-item">
-                    <a href="" class="sidebar-link">
-                        <span>Dashboard</span>
-                    </a>
-                 </li>
-                
-             </ul>
-         </aside>
-         <!-- fim da sidebar -->
-    </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
-  </body>
-</html>
+<!-- navegação da sidebar -->
+<ul class="sidebar-nav p-0">
+<!-- topo da navegação -->
+<li class="sidebar-header">
+    Paginas
+</li>
+<!-- itens da navegação -->
+    <li class="sidebar-item">
+    <a href="" class="sidebar-link">
+        <span>Dashboard</span>
+    </a>
+    </li>
+</ul>
+<!-- fim da navegação -->
+<!-- footer da navegação -->
+<div class="sidebar-footer">
+    <a href="" class="sidebar-link">
+        <span>Sair</span>
+    </a>
+</div>
+         
