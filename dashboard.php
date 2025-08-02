3c937b3e89638b346f6c97b8d5b88108d3625fe7
Jhonn-Vieira <jhonnatavieiradesouza124@gmail.com>
2025-08-02 12:25:54 -0300

    fix(dashboard): adiciona o link do font awesome e altera o icone do botão

--- dashboard.php
+++ dashboard.php
@@ -6,6 +6,7 @@
     <title>sidebar</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.0/css/all.min.css">
 </head>
   <body>
     <div class="d-flex">
@@ -18,7 +19,7 @@
            <div class="main">
             <nav class="navbar navbar-expand border-bottom">
                 <button class="toggler-btn" type="button">
-                    button
+                    <i class="fa-solid fa-bars"></i>
                 </button>
             </nav>
             <main class="p-3">
@@ -30,6 +31,7 @@
                 </main>
            </div>
     </div>
+    <script src="script.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
   </body>
 </html>
