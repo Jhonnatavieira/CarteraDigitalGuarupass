341921b9d779c2894249cbfb56aebe31935dc1a7
Jhonn-Vieira <jhonnatavieiradesouza124@gmail.com>
2025-08-02 14:41:27 -0300

    arruma a logica de responsividade
    adiciona uma função sidebarRezise que verifica o tamanho da tela e aplica as classes caso necessario.
    alem disso, faz com que ela seja ativada pelo rezing da tela

--- script.js
+++ script.js
@@ -1,23 +1,32 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
 
     const toggler = document.querySelector(".toggler-btn");
     const overlay = document.querySelector("#overlay");
-    
 
-    if (window.innerWidth <= 1170) {
-        sidebar.classList.add('collapsed');
+
+    function sidebarRezise() {
+        if (window.innerWidth <= 1280) {
+            document.querySelector("#sidebar").classList.add("collapsed");
+        } else { 
+            document.querySelector("#sidebar").classList.remove("collapsed");
+        }
     }
 
-    toggler.addEventListener("click", function(){
+    toggler.addEventListener("click", function () {
         document.querySelector("#sidebar").classList.toggle("collapsed");
         console.log()
     })
 
-    
+
     // Adiciona um listener para o clique no overlay
-    overlay.addEventListener("click", function() {
+    overlay.addEventListener("click", function () {
         // Garante que ele sempre feche, adicionando a classe
-        sidebar.classList.add("collapsed");
+        document.querySelector("#sidebar").classList.toggle("collapsed");
     });
-    
+
+    // Listener para o redimensionamento da tela
+    window.addEventListener('resize', sidebarRezise);
+
+    // Executa a verificação de tela no carregamento inicial da página
+    sidebarRezise();
 });
