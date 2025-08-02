6d68e63c6a07dcde3b31049a090130f7f00072fd
Jhonn-Vieira <jhonnatavieiradesouza124@gmail.com>
2025-08-02 14:24:18 -0300

    feat(script): adiciona a logica do overlay

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
 
     const toggler = document.querySelector(".toggler-btn");
+    const overlay = document.querySelector("#overlay");
     
+
+    if (window.innerWidth <= 1170) {
+        sidebar.classList.add('collapsed');
+    }
+
     toggler.addEventListener("click", function(){
         document.querySelector("#sidebar").classList.toggle("collapsed");
         console.log()
     })
+
+    // --- PEQUENA ADIÇÃO ---
+    // Adiciona um listener para o clique no overlay
+    overlay.addEventListener("click", function() {
+        // Garante que ele sempre feche, adicionando a classe
+        sidebar.classList.add("collapsed");
+    });
+    // --- FIM DA ADIÇÃO ---
 });
