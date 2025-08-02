ff72da59ecce5ea4921e12f69a8aa610ba12c635
Jhonn-Vieira <jhonnatavieiradesouza124@gmail.com>
2025-08-02 15:22:40 -0300

    fix(sidebar): arruma a aparencia do botão de sair

--- sidebar.php
+++ sidebar.php
@@ -1,20 +1,20 @@
 <!-- sidebar -->
-         
+
 <div class="sidebar-logo">
     <a href="#">Guarupass</a>
 </div>
 
 <!-- navegação da sidebar -->
 <ul class="sidebar-nav p-0">
-<!-- topo da navegação -->
-<li class="sidebar-header">
-    Paginas
-</li>
-<!-- itens da navegação -->
+    <!-- topo da navegação -->
+    <li class="sidebar-header">
+        Paginas
+    </li>
+    <!-- itens da navegação -->
     <li class="sidebar-item">
-    <a href="" class="sidebar-link">
-        <span>Dashboard</span>
-    </a>
+        <a href="" class="sidebar-link">
+            <span>Dashboard</span>
+        </a>
     </li>
 </ul>
 <!-- fim da navegação -->
@@ -24,4 +24,3 @@
         <span>Sair</span>
     </a>
 </div>
-         
