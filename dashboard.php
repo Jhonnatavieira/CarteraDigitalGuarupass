a065e9f85ff41cef05717c91cf55956772ece3be
Jhonn-Vieira <jhonnatavieiradesouza124@gmail.com>
2025-08-06 00:25:07 -0300

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -52,8 +52,9 @@
                                     <div class="col-md-5">
                                         <div class="custom-card p-3">
                                             <h6 class="mb-2 text-start">Cartões</h6>
-                                            <div class="card-section d-flex justify-content-center align-items-center flex-column">
-                                                
+                                            <div
+                                                class="card-section d-flex justify-content-center align-items-center flex-column">
+
                                                 <img class="img-fluid" src="./img/Cartão-png/Cidadão.png"
                                                     alt="Cartão Cidadão">
                                             </div>
@@ -83,114 +84,120 @@
 
                                 <div class="row">
                                     <!-- Histórico de Atividade -->
-                                <div class="custom-card mt-md-5">
-                                    <div class="activity-section" style="max-height: 315.19px;">
-                                        <div>
-                                            <h6 class="mb-3">Histórico de Atividade</h6>
-                                        </div>
+                                    <div class="custom-card my-5 mt-md-5">
+                                        <div class="activity-section" style="max-height: 315.19px;">
+                                            <div>
+                                                <h6 class="mb-3">Histórico de Atividade</h6>
+                                            </div>
 
-                                        <!-- Item 1 - Linha 552 -->
-                                        <div class="activity-item">
-                                            <div class="row w-100">
-                                                <div class="col-8 col-md-10">
-                                                    <div class="d-flex align-items-center flex-grow-1 gap-2">
-                                                        <div class="activity-icon bus-icon">
-                                                            <i class="bi bi-bus-front"></i>
-                                                        </div>
-                                                        <div class="activity-details">
-                                                            <h6 class="text-start" >Linha: 552</h6>
-                                                            <p>22/07/2025</p>
+                                            <!-- Item 1 - Linha 552 -->
+                                            <div class="activity-item">
+                                                <div class="row w-100">
+                                                    <div class="col-8 col-md-9">
+                                                        <div class="d-flex align-items-center flex-grow-1 gap-2">
+                                                            <div class="activity-icon bus-icon">
+                                                                <i class="bi bi-bus-front"></i>
+                                                            </div>
+                                                            <div class="activity-details">
+                                                                <h6 class="text-start">Linha: 552</h6>
+                                                                <p>22/07/2025</p>
+                                                            </div>
                                                         </div>
                                                     </div>
-                                                </div>
-                                                <div class="col-4 col-md-2 d-flex justify-content-end">
-                                                    <div class="activity-value value-negative">R$ 8,20</div>
+                                                    <div class="col-4 col-md-3 d-flex justify-content-end align-items-center">
+                                                        <div class="activity-value value-negative">R$ 8,20</div>
+                                                    </div>
                                                 </div>
                                             </div>
-                                        </div>
 
-                                        <!-- Item 2 - Pix -->
-                                        <div class="activity-item">
-                                            <div class="row w-100">
-                                                <div class="col-8 col-md-10">
-                                                    <div class="d-flex align-items-center flex-grow-1 gap-2">
-                                                        <div class="activity-icon pix-icon">
-                                                            <i class="bi bi-plus-lg"></i>
-                                                        </div>
-                                                        <div class="activity-details">
-                                                            <h6 class="text-start">Pix:</h6>
-                                                            <p>22/07/2025 às 17:30</p>
+                                            <!-- Item 2 - Pix -->
+                                            <div class="activity-item">
+                                                <div class="row w-100">
+                                                    <div class="col-8 col-md-9">
+                                                        <div class="d-flex align-items-center flex-grow-1 gap-2">
+                                                            <div class="activity-icon pix-icon">
+                                                                <i class="bi bi-plus-lg"></i>
+                                                            </div>
+                                                            <div class="activity-details">
+                                                                <h6 class="text-start">Pix:</h6>
+                                                                <p>22/07/2025 às 17:30</p>
+                                                            </div>
                                                         </div>
                                                     </div>
-                                                </div>
-                                                <div class="col-4 col-md-2 d-flex justify-content-end">
-                                                    <div class="activity-value value-positive">R$ 100,00</div>
+                                                    <div class="col-4 col-md-3 d-flex justify-content-end align-items-center">
+                                                        <div class="activity-value value-positive">R$ 100,00</div>
+                                                    </div>
                                                 </div>
                                             </div>
-                                        </div>
 
 
-                                        <!-- Item 3 - Linha 498 -->
-                                        <div class="activity-item">
-                                            <div class="row w-100">
-                                                <div class="col-8 col-md-10">
-                                                    <div class="d-flex align-items-center flex-grow-1 gap-2">
-                                                        <div class="activity-icon bus-icon">
-                                                            <i class="bi bi-bus-front"></i>
-                                                        </div>
-                                                        <div class="activity-details">
-                                                            <h6 class="text-start" >Linha: 552</h6>
-                                                            <p>22/07/2025</p>
+                                            <!-- Item 3 - Linha 498 -->
+                                            <div class="activity-item">
+                                                <div class="row w-100">
+                                                    <div class="col-8 col-md-9">
+                                                        <div class="d-flex align-items-center flex-grow-1 gap-2">
+                                                            <div class="activity-icon bus-icon">
+                                                                <i class="bi bi-bus-front"></i>
+                                                            </div>
+                                                            <div class="activity-details">
+                                                                <h6 class="text-start">Linha: 552</h6>
+                                                                <p>22/07/2025</p>
+                                                            </div>
                                                         </div>
                                                     </div>
-                                                </div>
-                                                <div class="col-4 col-md-2 d-flex justify-content-end">
-                                                    <div class="activity-value value-negative">R$ 8,20</div>
+                                                    <div class="col-4 col-md-3 d-flex justify-content-end align-items-center">
+                                                        <div class="activity-value value-negative">R$ 8,20</div>
+                                                    </div>
                                                 </div>
                                             </div>
-                                        </div>
 
-                                        <!-- Item 4 - Linha 498 -->
-                                        <div class="activity-item">
-                                            <div class="row w-100">
-                                                <div class="col-8 col-md-10">
-                                                    <div class="d-flex align-items-center flex-grow-1 gap-2">
-                                                        <div class="activity-icon bus-icon">
-                                                            <i class="bi bi-bus-front"></i>
-                                                        </div>
-                                                        <div class="activity-details">
-                                                            <h6 class="text-start" >Linha: 552</h6>
-                                                            <p>22/07/2025</p>
+                                            <!-- Item 4 - Linha 498 -->
+                                            <div class="activity-item">
+                                                <div class="row w-100">
+                                                    <div class="col-8 col-md-9">
+                                                        <div class="d-flex align-items-center flex-grow-1 gap-2">
+                                                            <div class="activity-icon bus-icon">
+                                                                <i class="bi bi-bus-front"></i>
+                                                            </div>
+                                                            <div class="activity-details">
+                                                                <h6 class="text-start">Linha: 552</h6>
+                                                                <p>22/07/2025</p>
+                                                            </div>
                                                         </div>
                                                     </div>
-                                                </div>
-                                                <div class="col-4 col-md-2 d-flex justify-content-end">
-                                                    <div class="activity-value value-negative">R$ 8,20</div>
+                                                    <div class="col-4 col-md-3 d-flex justify-content-end align-items-center">
+                                                        <div class="activity-value value-negative">R$ 8,20</div>
+                                                    </div>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
-                                </div>
                             </div>
 
                             <!-- Coluna Direita - Espaço para futuras funcionalidades e Gráfico -->
-                            <div class="col-lg-4 d-flex justify-content-center align-items-center flex-column gap-5">
-                                <!-- Card vazio para futuras funcionalidades -->
-                                <div class="custom-card mb-3 w-50" style="height: 200px;">
-                                    <!-- Espaço reservado para futuras funcionalidades -->
-                                </div>
+                            <div class="col-lg-4">
+                                <div class="row">
+                                    <div class="col-12 col-md-6 col-lg-12 d-flex justify-content-center align-items-center mb-lg-5">
+                                        <!-- Card vazio para futuras funcionalidades -->
+                                        <div class="custom-card" style="height: 225px; width: 18.75rem;">
+                                            <!-- Espaço reservado para futuras funcionalidades -->
+                                        </div>
+                                    </div>
 
-                                <!-- Gráfico Circular -->
-                                <div class="custom-card w-50" style="height: 200px;">
-                                    <div class="chart-container">
-                                        <div class="circular-chart">
-                                            <svg viewBox="0 0 100 100">
-                                                <!-- Círculo de fundo -->
-                                                <circle class="circle-bg" cx="50" cy="50" r="40"></circle>
-                                                <!-- Círculo de progresso -->
-                                                <circle class="circle" cx="50" cy="50" r="40"></circle>
-                                            </svg>
+                                    <div class="col-12 col-md-6 col-lg-12 d-flex justify-content-center align-items-center my-5 mt-lg-5">
+                                        <!-- Gráfico Circular -->
+                                        <div class="custom-card" style="height: 200px; width: 18.75rem;">
+                                            <div class="chart-container">
+                                                <div class="circular-chart">
+                                                    <svg viewBox="0 0 100 100">
+                                                        <!-- Círculo de fundo -->
+                                                        <circle class="circle-bg" cx="50" cy="50" r="40"></circle>
+                                                        <!-- Círculo de progresso -->
+                                                        <circle class="circle" cx="50" cy="50" r="40"></circle>
+                                                    </svg>
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
