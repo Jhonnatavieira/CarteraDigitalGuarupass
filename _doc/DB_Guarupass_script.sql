fdf97203f077b074ddee453845b0b6867a8bfe26
Andressa-Santiago <andressa.santiiago0@gmail.com>
2025-08-08 14:18:27 -0300

    Update DB_Guarupass_script.sql

--- _doc/DB_Guarupass_script.sql
+++ _doc/DB_Guarupass_script.sql
@@ -8,11 +8,11 @@ create table usuario
 (
 	id_usuario int not null auto_increment primary key,
     nome varchar(255) not null,
-    usuario varchar(100) not null,
+    usuario varchar(100) not null unique,
     senha varchar(60) not null,
-    cpf char(12) not null,
+    cpf char(12) not null unique,
     data timestamp not null,
-    email varchar(255) not null,
+    email varchar(255) not null unique,
     telefone char(15) not null,
     saldo decimal(10,2) not null,
     status enum('Ativo', 'Inativo') not null
@@ -23,7 +23,7 @@ create table cartao
 	id_cartao int not null auto_increment primary key,
     id_usuario_cartao int not null,
     tipo_cartao enum('Cidadão', 'Escolar', 'VT') not null,
-    codigo char(15) not null,
+    codigo char(15) not null unique,
     saldo decimal(10,2) not null,
     expira date not null,
     status enum('Ativo', 'Inativo') not null
@@ -44,9 +44,9 @@ create table transacoes
 (
 	id_transacoes int not null auto_increment primary key,
     id_usuario_transacoes int not null,
-    id_cartao int null,
-    id_qr int null,
-    tipo enum('Ativo', 'Inativo') not null,
+    id_cartao_transacoes int null,
+    id_qrcode__transacoes int null,
+    tipo enum('Debito', 'Pix') not null,
     valor decimal(10,2) not null,
     location varchar(255) not null,
     linha_onibus varchar(255) not null,
@@ -56,12 +56,40 @@ create table transacoes
 create table qrcode
 (
 	id_qrcode int not null auto_increment primary key,
-    id_qrcode int not null,
     id_usuario_qrcode int not null,
-    codigo_unico varchar(255) not null,
+    codigo_unico varchar(255) not null unique,
     data_geracao timestamp not null,
     data_expiracao datetime not null,
     valor decimal not null,
     status enum('Ativo', 'Inativo') not null
 );
+
+-- Olhar as tabelas
+
+select * from usuario;
+select * from cartao;
+select * from agendamento;
+select * from transacoes;
+select * from qrcode;
+
+-- FK
+
+alter table cartao 
+add constraint fk_id_usuario_cartao foreign key (id_usuario_cartao) references usuario (id_usuario);
+
+alter table agendamento 
+add constraint fk_id_usuario_agendamento foreign key (id_usuario_agendamento) references usuario (id_usuario);
+
+alter table  transacoes
+add constraint  fk_id_usuario_transacoes foreign key (id_usuario_transacoes) references usuario (id_usuario);
+
+alter table  transacoes
+add constraint  fk_id_cartao_transacoes foreign key (id_cartao_transacoes) references cartao (id_cartao);
+
+alter table  transacoes
+add constraint  fk_id_qrcode__transacoes foreign key (id_qrcode__transacoes) references qrcode (id_qrcode);
+    
+alter table  qrcode
+add constraint  fk_id_usuario_qrcode foreign key (id_usuario_qrcode) references usuario (id_usuario);
+
 
