adf81f19815752cfcf7518cef2c0a1f995dd2a7b
Jhonn-Vieira <jhonnatavieiradesouza124@gmail.com>
2025-08-13 14:32:39 -0300

    feat: cria o sistema de login

--- Controllers/login_controller.php
+++ Controllers/login_controller.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 session_start();
 
@@ -12,10 +12,26 @@ if ($_POST) {
     try {
         $usuario_obj = Usuario::buscarPorUsuarioOuEmail($identificador, $pdo);
 
-        
-    } catch (\Throwable $e) {
-        # code...
+        if ($usuario_obj && $usuario_obj->verificarSenha($senha_pura)) {
+            // SUCESSO!
+            session_regenerate_id(true);
+            $_SESSION['logado'] = true;
+            $_SESSION['usuario_id'] = $usuario_obj->getIdUsuario();
+            $_SESSION['usuario_nome'] = $usuario_obj->getNome();
+            $_SESSION['usuario_cpf'] = $usuario_obj->getCpf();
+
+            header('Location: ../Views/dashboard.php');
+            exit();
+        } else {
+            // FALHA!
+            $_SESSION['msg_erro_login'] = "Usuário/E-mail ou senha inválidos.";
+            header('Location: ../index.php');
+            exit();
+        }
+    } catch (Exception $e) {
+        error_log("Erro no login: " . $e->getMessage());
+        $_SESSION['msg_erro_login'] = "Ocorreu um erro inesperado. Tente novamente.";
+        header('Location: ../public/login.php');
+        exit();
     }
 }
-
-?>
